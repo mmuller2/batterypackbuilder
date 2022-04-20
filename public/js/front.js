b583dc5f43c8485873f84932d9deb0cb7a25f7ac
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2022-04-20 14:35:32 +0100

    added get by id vanilla js

--- public/js/front.js
+++ public/js/front.js
@@ -1,5 +1,9 @@
-const url = 'http://localhost:3000';
+// query select button
+// query select location of .. in the dom
+//add event listener (click) to do x once button is pressed
 //
+
+const url = 'http://localhost:3000';
 //
 //DOM manipulation
 const getBatteries = document.querySelector('#getPack');
@@ -9,9 +13,48 @@ const getIdealPack = document.querySelector('#three');
 const inputValue = document.querySelector('#inputValue');
 getBatteries.addEventListener('click', getAll);
 getIdealPack.addEventListener('click', getIdeal);
+//delete your pack events
+const deletePack = document.querySelector('#deletePack');
+deletePack.addEventListener('click', deleteAll);
 //
-/////////////////////////
-
+//
+///////////////////////// Batt by Id ////////////////////////
+// gets the number input for get battery by id
+const inputValueById = document.querySelector('#inputValueById');
+// creates DOM space for battery by id
+const creatingDOMelement3 = document.querySelector('#creatingDOMelement3');
+// event listener and button  to call battery by id
+const getBattId = document.querySelector('#getPackId');
+getBattId.addEventListener('click', getBattById);
+//
+// get battery by id
+async function getBattById() {
+  const response = await fetch(`${url}/batteries/${inputValueById.value}`);
+  const { payload } = await response.json();
+  console.log(payload);
+  const pickedByIdArr = payload.map((input) => {
+    byId(input.id, input.capacity);
+  });
+}
+//
+// creates element space in the DOM (get batteries by id)
+function byId(id, capacity) {
+  const idList = document.createElement('li');
+  idList.innerHTML = `<p>ID:${id}, Capacity:${capacity}</p>`;
+  creatingDOMelement3.appendChild(idList);
+  console.log('get batteries by id button pressed');
+}
+//////////////////////////
+//
+//gets the info from /batteries
+async function getAll() {
+  const response = await fetch(`${url}/batteries`);
+  const { payload } = await response.json();
+  console.log(payload);
+  const newArr = payload.map((input) => {
+    hello(input.id, input.capacity);
+  });
+}
 //
 // creates element space in the DOM (get all batteries)
 function hello(id, capacity) {
@@ -20,27 +63,6 @@ function hello(id, capacity) {
   creatingDOMelement.appendChild(list);
   console.log('get all batteries button pressed');
 }
-//
-//
-// creates element space in the DOM for get picked pack
-function pickedSpot(id, capacity) {
-  const pickedlist = document.createElement('li');
-  pickedlist.innerHTML = `<p>ID:${id}, Capacity:${capacity}</p>`;
-  creatingDOMelement2.appendChild(pickedlist);
-  console.log('get best pack button pressed');
-}
-//
-//
-//delete your pack events
-const deletePack = document.querySelector('#deletePack');
-deletePack.addEventListener('click', deleteAll);
-//
-//
-//add a new table
-const addTable = document.querySelector('#submitYourPack');
-const form = document.querySelector('#submitform');
-form.addEventListener('submit', handlerForm);
-//
 //
 // get picked Pack functions
 async function getIdeal() {
@@ -53,18 +75,18 @@ async function getIdeal() {
   });
 }
 //
-// get all batteries functions
-//gets the info from /batteries
-async function getAll() {
-  const response = await fetch(`${url}/batteries`);
-  const { payload } = await response.json();
-  console.log(payload);
-  const newArr = payload.map((input) => {
-    hello(input.id, input.capacity);
-  });
+// creates element space in the DOM (get picked pack)
+function pickedSpot(id, capacity) {
+  const pickedlist = document.createElement('li');
+  pickedlist.innerHTML = `<p>ID:${id}, Capacity:${capacity}</p>`;
+  creatingDOMelement2.appendChild(pickedlist);
+  console.log('get best pack button pressed');
 }
-
 //
+//add a new table
+const addTable = document.querySelector('#submitYourPack');
+const form = document.querySelector('#submitform');
+form.addEventListener('submit', handlerForm);
 //
 //delete your pack functions
 async function deleteAll() {
@@ -73,14 +95,10 @@ async function deleteAll() {
     .then((data) => (window.location.href = data.redirect));
 }
 //
-//
 //Intercept the flowFrom:
-
 function handlerForm(event) {
   event.preventDefault();
-
   const data = new FormData(event.target);
-
   const value = data.getAll('capacity');
   value.map((capacity) => {
     if (capacity === '') {
@@ -88,13 +106,12 @@ function handlerForm(event) {
     } else {
       const stringToNumber = Number(capacity);
       const body = { capacity: stringToNumber };
-
       fetchCreate(body);
     }
   });
-
   console.log({ value });
 }
+//
 //CREATE new batteries
 async function fetchCreate(body) {
   const fetchResponse = await fetch(`${url}/batteries`, {
